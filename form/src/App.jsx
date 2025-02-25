585223ebc34a38fad8db854b620e4bd86a2d77f9
chiaralle <chiaraallegrino@gmail.com>
2025-02-25 21:35:15 +0100

    add: form

--- form/src/App.jsx
+++ form/src/App.jsx
@@ -8,10 +8,10 @@ Implementare la funzionalità di modifica del titolo di un post. */
 
 import { useState } from "react"
 const articles = [
-  {title:"5 Ricette Veloci per Una Cena Salutare e Gustosa"},
-  {title:"Come Preparare il Pane Fatto in Casa: La Guida Definitiva"},
-  {title:"Come Sfruttare al Meglio le Spezie in Cucina"},
-  {title:"Dolci Senza Glutine: 7 Dessert Facili e Deliziosi"}
+  "5 Ricette Veloci per Una Cena Salutare e Gustosa",
+  "Come Preparare il Pane Fatto in Casa: La Guida Definitiva",
+  "Come Sfruttare al Meglio le Spezie in Cucina",
+  "Dolci Senza Glutine: 7 Dessert Facili e Deliziosi"
 ]
 
 function App() {
@@ -20,18 +20,40 @@ function App() {
   /*articolo nuovo da aggiungere all'array iniziale */
   const [newTitle, setNewTitle] = useState('')
 
+  const addTitle = (e) => {
+    e.preventDefault()
+    const article= newTitle.trim()
+    setTitle( [...title, article] )
+    setNewTitle('')
+  }
+
 
   return (
     <>
     <div className="container">
       <h1>I nostri articoli</h1>
+      
       <ul>
         {
-          articles.map((article, index) => (
-            <li key={index}>{article.title}</li>
-          ))
+          articles.map((article, index) => {
+            return (
+            <li key={index}>{article}</li>
+          )
+        })
         }
       </ul>
+
+      {/*form per aggiungere un nuovo articolo */}
+      <form onSubmit={addTitle}>
+        <h4>Aggiungi il tuo articolo</h4>
+        <input type="text"
+         className="form-control" 
+         placeholder="Aggiungi qui il tuo articolo"
+          value={newTitle} 
+          onChange={(e) => setNewTitle(e.target.value)}
+          />
+        <button>Aggiungi</button>
+      </form>
 
       
     </div>
